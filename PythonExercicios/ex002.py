0c2bd5af1a2ca962e54f3c5b087fec2f198c137c
liatsuki <lidia.duarte.goncalves@gmail.com>
2026-07-14 07:28:38 +0100

    140726_0728

---
+++ PythonExercicios/ex002.py
@@ -0,0 +1,5 @@
+nome = input('Digite seu nome: ')
+print('E um prazer te conhecer, {}!'.format(nome))
+
+# Desafio 002
+# Faca um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
---
+++ PythonExercicios/ex003.py
@@ -0,0 +1,8 @@
+# Crie um programa que leia dois numeros e mostre a soma entre eles.
+
+n1 = int(input('Numero 1: '))
+n2 = int(input('Numero 2: '))
+
+soma = n1 + n2
+
+print("A soma entre {} e {} vale {}.".format(n1, n2, soma))
