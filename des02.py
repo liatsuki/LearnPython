50c45bdf4b92dafd3c2a5ef45253977c6fdaa6bb
liatsuki <lidia.duarte.goncalves@gmail.com>
2026-08-06 14:01:52 +0100

    060826_1400

---
+++ des02.py
@@ -0,0 +1,9 @@
+# Crie um script Python que leia o dia, o mes e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada.
+
+print('====== DESAFIO 02 ======')
+
+dia = input('Dia? ')
+mes = input('Mes? ')
+ano = input('Ano? ')
+
+print('Voce nasceu no dia ' + dia + ' de ' + mes + ' de ' + ano + '. Correto?')
---
+++ des03.py
@@ -0,0 +1,10 @@
+# Crie um script Python que leia dois numeros e tente mostrar a soma entre eles.
+
+print('====== DESAFIO 03 ======')
+
+n1 = int(input("Num 1? "))
+n2 = int(input("Num 2? "))
+
+soma = n1 + n2
+
+print('Soma =', soma)
