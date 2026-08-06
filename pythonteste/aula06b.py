2a8b3c55dc69d071a8bf0d4d79d6111bf343c6a3
liatsuki <lidia.duarte.goncalves@gmail.com>
2026-08-06 16:30:39 +0100

    060826_1630

---
+++ pythonteste/aula06b.py
@@ -0,0 +1,14 @@
+n1 = float(input('n1: '))
+print(type(n1))
+print(n1)
+
+n2 = bool(input('n2: '))
+print(type(n2))
+print(n2)
+
+n = input('n: ')  # metodos de teste de tipos
+print(n.isnumeric())  # numero
+print(n.isalpha())    # alfabetico
+print(n.isalnum())    # alfanumerico
+print(n.isupper())    # letras maisculas
+
