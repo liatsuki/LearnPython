6f700c61fd1d231d5bfd3dc492cc3642ea99ee9d
liatsuki <lidia.duarte.goncalves@gmail.com>
2026-08-15 20:16:05 +0100

    150826_2016

--- pythondesafio/ex022.py
+++ pythondesafio/ex022.py
@@ -3,3 +3,14 @@
     # - O nome com todas minusculas
     # - Quantas letras ao todo (sem considerar espacos)
     # - Quantas letras tem o primeiro nome
+
+nome = input('Nome: ')
+
+print(nome.upper())
+print(nome.lower())
+
+# print()
+
+dividido = nome.split()
+nome1 = dividido[1]
+print(len(nome1))
